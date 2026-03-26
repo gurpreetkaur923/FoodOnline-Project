957cc78ee8f40fa2d1891cc0fdc7063d0ca03936
gurpreetkaur923 <gurpreetkdhaliwal92@gmail.com>
2026-03-26 21:14:09 +0000

    vendor registration setup

--- foodOnline_m/settings.py
+++ foodOnline_m/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
+    'vendor',
 ]
 
 MIDDLEWARE = [
@@ -134,3 +135,11 @@ MEDIA_ROOT = BASE_DIR /'media'
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.ERROR: "danger",
+   
+}
